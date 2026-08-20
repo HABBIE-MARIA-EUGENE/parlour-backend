bc646cf7f712faca761e6901f024fa4638103ce8
HABBIE-MARIA-EUGENE <meugenem1409@gmail.com>
2026-08-20 09:31:32 +0530

    main.py cords - middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,8 +17,17 @@ from app.routes.gallery import (
 
 
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount(
     "/uploads",
